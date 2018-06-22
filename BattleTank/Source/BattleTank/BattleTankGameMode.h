a92b992cedae0da60c27fa3f469a1e03dee3c6ad
pv62 <pranav.v99@hotmail.com>
2018-06-22 17:52:00 +0530

    Configuring a tank

---
+++ BattleTank/Source/BattleTank/BattleTankGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BattleTankGameModeBase.h"
+#include "BattleTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameMode : public ABattleTankGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
