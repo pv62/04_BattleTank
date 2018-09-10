c3e8fca49cc337fb5c6b649a391935bd2ec96ad9
pv62 <pranav.v99@hotmail.com>
2018-09-10 17:17:56 +0800

    BlueprintImplementableEvent

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,11 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Pranav.
 
 #include "TankPlayerController.h"
 #include "Tank.h"
+#include "TankAimingComponent.h"
 
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
+	if (AimingComponent)
+	{
+		FoundAimingComponent(AimingComponent);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller can't find Aiming Component at BeginPlay"));
+	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
