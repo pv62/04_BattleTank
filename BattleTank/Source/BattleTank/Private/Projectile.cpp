73f4ae75d668a11f86fbd7df41279f4d509168b3
pv62 <pranav.v99@hotmail.com>
2018-09-18 17:40:38 +0800

    Using GetTimerManger()

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -1,6 +1,7 @@
 // Copyright Pranav.
 
 #include "Projectile.h"
+#include "TimerManager.h"
 #include "Engine/World.h"
 
 // Sets default values
@@ -46,5 +47,15 @@ void AProjectile::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor,
 	LaunchBlast->Deactivate();
 	ImpactBlast->Activate();
 	ExplosionForce->FireImpulse();
+
+	SetRootComponent(ImpactBlast);
+	CollisionMesh->DestroyComponent();
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
 }
 
+void AProjectile::OnTimerExpire()
+{
+	Destroy();
+}
